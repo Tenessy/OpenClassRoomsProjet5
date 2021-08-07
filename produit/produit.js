2bc69dc1d022ed0b13d8954706ec78fe493958b8
Tenessy <tendu35@hotmail.fr>
2021-08-07 10:10:08 +0200

    produit

--- produit/produit.js
+++ produit/produit.js
@@ -1,9 +1,12 @@
 
 fetch("http://localhost:3000/api/teddies/" + getUrl())
- .then(data => data.json())
+.then(function(res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
   .then(teddy => {
         let teddyId = new Teddy(teddy);
-        if (getUrl() == teddyId._id) {
           document.querySelector(".container").innerHTML = 
           `<div class="card" style="width: 18rem;">
           <img src="${teddyId.imageUrl}" class="card-img-top" alt="...">
@@ -12,36 +15,43 @@ fetch("http://localhost:3000/api/teddies/" + getUrl())
             <h6 class="card-subtitle mb-2 text-muted">${teddyId.price}€</h6>
             <select class="form-select selected"></select>
             <p class="card-text">${teddyId.description}</p>
-            <a href="../panier/panier.html" class="btn btn-primary ajout" data-id="${teddyId._id}">Ajouter au panier</a>
+            <a href="#" class="btn btn-primary ajout" data-id="${teddyId._id}">Ajouter au panier</a>
           </div>
         </div>` 
-        }
-        else {
-          document.querySelector(".container").innerHTML = 
-          `<h1>Erreur 404 page introuvable</h1>`
-        }
-        
-       color();
 
-      function color() {
-        let teddyColor = teddyId.colors;
-        let select = document.querySelector(".selected");
-        teddyColor.forEach(color => {
-           select.innerHTML += '<option value=' + color + '>' + color + '</option>';        
-       })
-       select.addEventListener("change", function(e){
-        recupColor(e.target.value);
-           
-       })
+        let arrayProducts = addProducts();
 
+        function addProducts() {
+          let arrayProducts = [];
+           const id = teddy._id;
+           const name = teddy.name;
+           const price = teddy.price;
+           const image = teddy.imageUrl;
+           arrayProducts.push(id,name,price,image);
+
+          return arrayProducts;
+        }
+        console.log(addProducts());
+
+        function color() {
+          let select = document.querySelector(".selected");
+          let teddyColor = teddy.colors
+          for (let i =0; i < teddyColor.length; i++) {
+            console.log(teddyColor[i])
+            let option = document.createElement("option");
+            option.value = teddyColor[i];
+            option.text = teddyColor[i];
+            select.appendChild(option);   
+          } 
+        }
+        color();
        document.querySelectorAll(".ajout").forEach(ajouter => {
          ajouter.addEventListener("click", function(e) {
-             articlePanier(this.dataset.id);
+         //    addProduct(teddyId);     
+         addProduct(arrayProducts);
+             e.preventDefault();
          })
        })
-    }
-         
-     
- 
-      
 })
+
+
