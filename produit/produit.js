5807e4539ac4d01a4e42fbb1bb30fceb94e904d7
Tenessy <tendu35@hotmail.fr>
2021-08-09 23:18:17 +0200

    produit

--- produit/produit.js
+++ produit/produit.js
@@ -1,38 +1,37 @@
-
-fetch("http://localhost:3000/api/teddies/" + getUrl())
+const getUrlId = getUrl();
+fetch("http://localhost:3000/api/teddies/" + getUrlId)
 .then(function(res) {
   if (res.ok) {
     return res.json();
   }
+  else {
+    document.querySelector(".container").innerHTML =
+          "<h1>Erreur 404 page introuvable</h1>";
+          console.log("Erreur 404 page introuvable");
+  }
 })
   .then(teddy => {
-        let teddyId = new Teddy(teddy);
+        let ted = new Teddy(teddy);
+        if(ted._id === getUrlId) {
           document.querySelector(".container").innerHTML = 
           `<div class="card" style="width: 18rem;">
-          <img src="${teddyId.imageUrl}" class="card-img-top" alt="...">
+          <img src="${ted.imageUrl}" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">${teddyId.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${teddyId.price}€</h6>
+            <h5 class="card-title">${ted.name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${ted.price}€</h6>
             <select class="form-select selected"></select>
-            <p class="card-text">${teddyId.description}</p>
-            <a href="#" class="btn btn-primary ajout" data-id="${teddyId._id}">Ajouter au panier</a>
+            <p class="card-text">${ted.description}</p>
+            <div class="col text-center">
+              <a href="#" class="btn btn-primary ajout" data-id="${ted._id}">Ajouter au panier</a>
+            </div>
           </div>
         </div>` 
-
-        let arrayProducts = addProducts();
-
-        function addProducts() {
-          let arrayProducts = [];
-           const id = teddy._id;
-           const name = teddy.name;
-           const price = teddy.price;
-           const image = teddy.imageUrl;
-           arrayProducts.push(id,name,price,image);
-
-          return arrayProducts;
         }
-        console.log(addProducts());
-
+        else {
+          document.querySelector(".container").innerHTML =
+          "<h1>Erreur 404 page introuvable</h1>";
+          console.log("Erreur 404 page introuvable");
+       }    
         function color() {
           let select = document.querySelector(".selected");
           let teddyColor = teddy.colors
@@ -45,13 +44,11 @@ fetch("http://localhost:3000/api/teddies/" + getUrl())
           } 
         }
         color();
-       document.querySelectorAll(".ajout").forEach(ajouter => {
-         ajouter.addEventListener("click", function(e) {
-         //    addProduct(teddyId);     
-         addProduct(arrayProducts);
-             e.preventDefault();
-         })
-       })
+        document.querySelectorAll(".ajout").forEach(ajouter => {
+          ajouter.addEventListener("click", function(e) {
+             addProduct(teddy);
+          })
+        })
 })
 
 
