c35e05e4d8fe41da41acd8e40cb0d4b38764144f
Tenessy <tendu35@hotmail.fr>
2021-08-11 11:18:59 +0200

    panier

--- panier/panier.js
+++ panier/panier.js
@@ -3,23 +3,24 @@ const sommePrix = getSum();
 const panierVide = paniers.length === 0;
 
 document.getElementById("titre").innerHTML = panierVide ? "Votre panier Orinoco est vide" : "Votre panier";
-document.getElementById("prix").innerHTML = sommePrix;
+document.getElementById("prix").innerHTML = panierVide ? "" : sommePrix;
 
 for(let panier of paniers) {
     let teddyPanier = new Teddy(panier);
      document.querySelector(".container").innerHTML += 
-    ` <div class="card" style="width: 18rem;">
+    ` <div class="card update" style="width: 18rem;">
      <img src="${teddyPanier.imageUrl}" class="card-img-top" alt="...">
      <div class="card-body">
        <h5 class="card-title">${teddyPanier.name}</h5>
        <h6 class="card-subtitle mb-2 text-muted">${teddyPanier.price}€</h6>
        <p class="card-text">${teddyPanier.description}</p>
-       <select class="form-select selected" data-id="${teddyPanier._id}"aria-label="Default select example">
+       <select class="form-select selected"aria-label="Default select example">
          <option value="1">1</option>
          <option value="2">2</option>
          <option value="3">3</option>
         <option value="4">4</option>
        </select>
+       <a class="btn btn-primary suppr" data-id="${teddyPanier._id}" href="#">Supprimer</a>
      </div>
    </div></br>` 
    function compte(){
@@ -29,11 +30,15 @@ for(let panier of paniers) {
           let somme = 0
           somme += valeur * e.target.teddyPanier.price;
           console.log(somme);
-       })
+       });
 
    }
    compte();
 
+  document.querySelectorAll(".suppr").forEach((product,i) => product.addEventListener("click", () =>  deleteProduct(i)))
 }
+
+    
+
 
 
