e78745b6cb9927f366a3e7dcdc86a026d484d25a
Tenessy <tendu35@hotmail.fr>
2021-08-13 23:00:19 +0200

    panier

--- panier/panier.js
+++ panier/panier.js
@@ -1,44 +1,70 @@
-const paniers = getPanier();
-const sommePrix = getSum();
-const panierVide = paniers.length === 0;
 
-document.getElementById("titre").innerHTML = panierVide ? "Votre panier Orinoco est vide" : "Votre panier";
-document.getElementById("prix").innerHTML = panierVide ? "" : sommePrix;
+const paniers = getPanier();
+const panierVide = paniers.length === 0;
+const getSomme = getSum();
+//let titre= document.getElementById("titre").innerHTML = panierVide ? "Votre panier Orinoco est vide" : "Votre panier";
+let prix =document.getElementById("prix");
+let corps = document.querySelector(".corps");
+if (panierVide) {
+  corps.innerHTML = `
+      <div class="card">
+        <img src= "../PanierVide.png" />
+        <a class="btn btn-primary" href="../index.html" role="button">Continuez vos achats</a>
+      </div></br>`
+      titre.innerHTML = "Votre panier Orinoco est vide";
+
+}
+else {
+  prix.innerHTML = getSomme;
+  titre.innerHTML = "Votre panier";
+  console.log("OK");
+}
 
 for(let panier of paniers) {
     let teddyPanier = new Teddy(panier);
-     document.querySelector(".container").innerHTML += 
+     document.querySelector(".carte").innerHTML +=
     ` <div class="card update" style="width: 18rem;">
      <img src="${teddyPanier.imageUrl}" class="card-img-top" alt="...">
      <div class="card-body">
        <h5 class="card-title">${teddyPanier.name}</h5>
-       <h6 class="card-subtitle mb-2 text-muted">${teddyPanier.price}€</h6>
+       <h6 class="card-subtitle mb-2 text-muted price">${teddyPanier.price}€</h6>
        <p class="card-text">${teddyPanier.description}</p>
-       <select class="form-select selected"aria-label="Default select example">
+       <h6>Qté</h6> <select class="form-select selected" data-id="${teddyPanier._id}" aria-label="Default select example">
          <option value="1">1</option>
          <option value="2">2</option>
          <option value="3">3</option>
         <option value="4">4</option>
        </select>
-       <a class="btn btn-primary suppr" data-id="${teddyPanier._id}" href="#">Supprimer</a>
+       <a class="suppr" href="#">Supprimer</a>
      </div>
    </div></br>` 
-   function compte(){
-       document.querySelector(".selected").addEventListener("change", function(e){
-           let valeur = e.target.value;
-           console.log(teddyPanier._id);
-          let somme = 0
-          somme += valeur * e.target.teddyPanier.price;
-          console.log(somme);
-       });
-
-   }
-   compte();
-
-  document.querySelectorAll(".suppr").forEach((product,i) => product.addEventListener("click", () =>  deleteProduct(i)))
+   document.querySelectorAll(".suppr").forEach((product,i) => product.addEventListener("click", () =>  deleteProduct(i)))
 }
 
-    
+let somme = 0;
+somme += panier.price;
+  document.querySelectorAll(".selected").forEach(select => {
+    select.addEventListener("change", function(e){
+      let valeur = 0;
+      valeur += e.target.value;
+      console.log(valeur);
+      console.log(this.dataset.id);
+      if(valeur === undefined) {
+        valeur = 1;
+      }
+      for(let i = 0; i <paniers.length; i++) {
+       if(this.dataset.id === paniers[i]._id) {
+         console.log(paniers[i].price);
+        const sommeTotale = getSomme + (paniers[i].price * valeur) - paniers[i].price;
+        console.log(sommeTotale);
+         
+       }
+      }
+      console.log(getSomme * valeur);
+        
+    });
+   });  
+
 
 
 
