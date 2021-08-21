cce50d16cf77418b7a8e35640045b5840884605d
Tenessy <tendu35@hotmail.fr>
2021-08-21 18:09:54 +0200

    confirmation

--- confirmation/confirmation.js
+++ confirmation/confirmation.js
@@ -1,6 +1,31 @@
 const productId = productsId();
 const objContact = getContact();
- 
+const getSomme = getSum();
+
+let today= new Date();
+const jours = today.getDate();
+const mois = today.getMonth();
+const annee = today.getFullYear();
+const tabMois = ["janvier", "février", "mars", "avril", 
+"mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"];
+let heure = today.getHours();
+let minutes = today.getMinutes();
+function duJour() {
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if(minutes > 59) {
+    minutes = "00";
+  }
+  if (heure < 10 ) {
+    heure = "0" + heure;
+  }
+  if (heure > 23) {
+    heure = "00";
+  }
+  return jours + " " + tabMois[mois] + " " + annee + " à " + heure + ":" + minutes;
+}
+
 fetch("http://localhost:3000/api/teddies/order", 
 {
     method: "POST",
@@ -19,23 +44,35 @@ fetch("http://localhost:3000/api/teddies/order",
  .then(orders => {
      console.log(orders.products);
     orders.products.forEach(product => {
-        document.querySelector(".carte").innerHTML +=
+        document.querySelector(".cartes").innerHTML +=
         `<div class="confirmation_size">
           <img src="${product.imageUrl}" class="card-img-top confirmation_size__img"  alt="...">
         <div class="card-body">
-          <h5 class="card-title">${product.name}</h5>
+          <h5 class="card-title kalam gras">${product.name}</h5>
           <h5>Qté: 1</h5>
-          <h6 class="card-subtitle mb-2 text-muted">${product.price}€</h6>
+          <h6 class="card-subtitle primary gras">${product.price.toLocaleString()}€</h6>
         </div>
+        <div class="confirmation__border_inf"></div>
       </div></br>`
     })
 
-    document.getElementById("orderId").innerHTML = `<div class="h6">Numéro de commande: ${orders.orderId}</div>`;
+    document.getElementById("orderId").innerHTML = `<h4>Commande n°: <strong>${orders.orderId}</strong></h4>
+    <h5>Effectué le <strong>${duJour()}</strong></h5>`;
     document.getElementById("orderCustomer").innerHTML = 
-    `<div class= "alert alert-success" role="alert"> Félicitations ${orders.contact.firstName} pour votre commande chez Orinoco !</div>`;
-    document.getElementById("somme").innerHTML = getSum();
+    `<div class= "alert alert-success" role="alert"> Félicitations <strong>${orders.contact.lastName}</strong> pour votre commande chez Orinoco !</div>`;
+   
+    document.querySelector(".containPrix").innerHTML = `<p>Sous-total des articles: ${getSomme}<br>
+    Frais d'envoie: 0€<br>
+    Total HT: ${getSomme}<br>
+    <strong>Montant total de la commande: ${getSomme}</strong></p> `;
+   
     document.querySelector(".envoieMail").innerHTML = `Un E-mail de confirmation vous a été 
-    envoyé à l'adresse suivant: ${orders.contact.email}`;
+    envoyé à l'adresse suivante: <strong>${orders.contact.email}.</strong><br>
+    À bientôt !`;
+   
+    document.querySelector(".livraison").innerHTML = `<h4 class="kalam">Votre commande sera livrée à</h4>
+    <strong>${orders.contact.address}</strong><br>
+    <strong>${orders.contact.city}</strong>`
 
  });
    
