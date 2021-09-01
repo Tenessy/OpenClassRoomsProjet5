2432f0e350521aba26f857ffe6ee85321a410eca
Tenessy <tendu35@hotmail.fr>
2021-09-01 17:31:48 +0200

    confirmation

--- confirmation/confirmation.js
+++ confirmation/confirmation.js
@@ -27,41 +27,40 @@ fetch("http://localhost:3000/api/teddies/order",
       document.getElementById("structurer").innerHTML = "Le panier est vide";
       console.log("ok")
     }
-   else {
-    orders.products.forEach(product => {
-      document.querySelector(".cartes").innerHTML +=
-      `<div class="confirmation_size">
-        <img src="${product.imageUrl}" class="card-img-top confirmation_size__img"  alt="...">
-      <div class="card-body">
-        <h5 class="card-title kalam gras">${product.name}</h5>
-        <h5>Qté: 1</h5>
-        <h6 class="card-subtitle primary gras">${product.price.toLocaleString()}€</h6>
-      </div>
-    </div>
-    <div class="confirmation__border_inf"></div></br>`
-
-    });
-    document.getElementById("orderId").innerHTML = `<h3>Commande n°: <strong>${orders.orderId}</strong></h3>
-    <h4>Effectué le <strong>${dateDuJour}</strong></h4>`;
-    document.getElementById("orderCustomer").innerHTML = 
-    `<h1> Félicitations <strong>${orders.contact.lastName}</strong> pour votre commande chez Orinoco !</h1>`;
+    else {
+      orders.products.forEach(product => {
+        document.querySelector(".cartes").innerHTML +=
+        `<div class="confirmation_size">
+          <img src="${product.imageUrl}" class="card-img-top confirmation_size__img"  alt="...">
+          <div class="card-body">
+           <h5 class="card-title kalam gras">${product.name}</h5>
+           <h5>Qté: 1</h5>
+           <h6 class="card-subtitle primary gras">${product.price.toLocaleString()}€</h6>
+          </div>
+        </div>
+        <div class="confirmation__border_inf"></div></br>`
+      });
+      document.getElementById("orderId").innerHTML = `<h3>Commande n°: <strong>${orders.orderId}</strong></h3>
+      <h4>Effectué le <strong>${dateDuJour}</strong></h4>`;
+      document.getElementById("orderCustomer").innerHTML = 
+     `<h1> Félicitations <strong>${orders.contact.lastName}</strong> pour votre commande chez Orinoco !</h1>`;
    
-    document.querySelector(".containPrix").innerHTML = `<p>Sous-total des articles: ${getSomme}<br>
-    Frais d'envoie: 0€<br>
-    Total HT: ${getSomme}<br>
-    <strong>Montant total de la commande: <mark class="primary">${getSomme}</mark></strong></p> `;
+      document.querySelector(".containPrix").innerHTML = `<p>Sous-total des articles: ${getSomme}<br>
+      Frais d'envoie: 0€<br>
+      Total HT: ${getSomme}<br>
+      <strong>Montant total de la commande: <mark class="primary">${getSomme}</mark></strong></p> `;
    
-    document.querySelector(".envoieMail").innerHTML = `Un E-mail de confirmation vous a été 
-    envoyé à l'adresse suivante: <strong>${orders.contact.email}.</strong><br>
-    À bientôt !`;
+      document.querySelector(".envoieMail").innerHTML = `Un E-mail de confirmation vous a été 
+      envoyé à l'adresse suivante: <strong>${orders.contact.email}.</strong><br>
+      À bientôt !`;
    
-    document.querySelector(".livraison").innerHTML = `<h4 class="kalam">Votre commande sera livrée à<br>
-    <strong>${orders.contact.address}</strong><br>
-    <strong>${orders.contact.city}</strong></h4>`
-  document.querySelector(".navbar").addEventListener("click", function (event){
-    localStorage.clear();
-  })
-  }
+      document.querySelector(".livraison").innerHTML = `<h4 class="kalam">Votre commande sera livrée à<br>
+      <strong>${orders.contact.address}</strong><br>
+      <strong>${orders.contact.city}</strong></h4>`
+       document.querySelector(".navbar").addEventListener("click", function (event) {
+        localStorage.clear();
+      });
+    }
 });
    
 
