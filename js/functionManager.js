970ed0201743167ca8e1b7cd934b5e4e03b12488
Tenessy <tendu35@hotmail.fr>
2021-09-07 21:53:16 +0200

    js

--- js/functionManager.js
+++ js/functionManager.js
@@ -3,17 +3,13 @@ function getUrl(id) {
     let Url = new URL(window.location);
     Url.pathname = "produit/produit.html";
     const searchId = Url.searchParams.get("id");
-    console.log(id)
-    return searchId; //retourne l'id contenu dans l'URL;
-}
-function isValid(value) {
-    return /^[a-zA-Z]+$/.test(value);
-}
-function isValidMail(value) {
-    return /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,4}$/.test(value);
-}
-function isValidAdress(value) {
-    return /[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+/.test(value);
+    console.log(id);
+    if (Url.searchParams.has("id")) {
+        return searchId; //retourne l'id contenu dans l'URL;
+    }
+    else {
+        console.log("l'id n'est pas trouvé");
+    }
 }
 
 function duJour() {
@@ -23,9 +19,7 @@ function duJour() {
         month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'
     }
     const date = today.toLocaleString('fr-FR', options); // transforme la date au format local
-
-    if (isNaN(Date.parse(date)) === false) {  // Vérifie que la date renvoie un nombre
-        console.log("la Date est OK");
+    if (isNaN(Date.now(date)) === false) {  // Vérifie que la date renvoie un nombre
         return date; // retourne la date au format FR et qui contient toutes les informations contenu dans l'objet options
     }
     else {
